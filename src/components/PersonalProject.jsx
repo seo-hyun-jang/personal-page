1f93c62a106dba08419e656906a781359092f54b
seohyun <12503309@iscu.ac.kr>
2026-02-12 09:32:25 +0900

    lastreall

--- src/components/PersonalProject.jsx
+++ src/components/PersonalProject.jsx
@@ -4,8 +4,8 @@ import TextAni from "./svgani/TextAni.jsx";
 export default function PersonalProject() {
   const project = {
     image: '/img/travus.png',
-    title: "TRAVEL PLANNER APP PROJECT 'TRAVIUS'",
-    description: [["TRAVIUS는 여행 준비부터 실행까지의 과정을 하나의 흐름으로 연결하는 AI 기반 여행 플래너 서비스입니다.", "기획부터 UX·UI 설계까지 전 과정을 단독으로 진행했습니다."],
+    title: "TRAVEL PLANNER APP PROJECT 'TRAVUS'",
+    description: [["TRAVUS는 여행 준비부터 실행까지의 과정을 하나의 흐름으로 연결하는 AI 기반 여행 플래너 서비스입니다.", "기획부터 UX·UI 설계까지 전 과정을 단독으로 진행했습니다."],
     ["여행 준비 단계의 정보 탐색·선택 피로를 문제로 정의하고, 사용자 여정 중심으로 화면 흐름을 재구성했습니다.", "각 단계에서 필요한 정보만 자연스럽게 이어지도록 설계해,", "사용자가 고민 없이 다음 행동으로 이동할 수 있는 경험을 만드는 데 집중했습니다."]],
     linkPlan: 'https://www.figma.com/design/NVNNJxevXpC6TpXoN6YAV0/23.%EC%9E%A5%EC%84%9C%ED%98%84?node-id=1556-17795&t=e4iKN61IgSATVm9C-1',
     linkProject: 'https://www.figma.com/design/NVNNJxevXpC6TpXoN6YAV0/23.%EC%9E%A5%EC%84%9C%ED%98%84?node-id=0-1&t=e4iKN61IgSATVm9C-1',
